4d385094396a10060167e5c4a2c7189663a94bcb
saimukzi <saimukzi@proton.me>
2022-10-19 10:12:18 +0800

    show eat detection on

--- Mobs/MobBase/MobBase.gd
+++ Mobs/MobBase/MobBase.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+func _ready():
+	$EatDetectionDisplay.hide()
+
 func _on_EatDetection_area_entered(area):
-	print("_on_EatDetection_area_entered %s"%area.name)
+	# print("_on_EatDetection_area_entered %s"%area.name)
+	$EatDetectionDisplay.show()
+
+func _on_EatDetection_area_exited(area):
+	$EatDetectionDisplay.hide()
