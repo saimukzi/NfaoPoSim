cd24c19d97e254e2c0bb6201f0a2bd54d16f3ad0
saimukzi <saimukzi@proton.me>
2022-10-22 18:53:04 +0800

    Mob Spawner spawn RandomMoveMob

--- DebugStage/MobSpawner.gd
+++ DebugStage/MobSpawner.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 export(NodePath) var map
-var mob_base_scene : PackedScene
+#var mob_base_scene : PackedScene
+var random_move_mob_scene : PackedScene
 
 func _ready():
-	mob_base_scene = preload("res://Mobs/MobBase/MobBase.tscn")
+#	mob_base_scene = preload("res://Mobs/MobBase/MobBase.tscn")
+	random_move_mob_scene = preload("res://Mobs/RandomMoveMob/RandomMoveMob.tscn")
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("debug_spawn"):
-		var mob_instance = mob_base_scene.instance() as Node2D
+#		var mob_instance = mob_base_scene.instance() as Node2D
+		var mob_instance = random_move_mob_scene.instance() as Node2D
 		mob_instance.position = position
 		get_node(map).add_child(mob_instance)
