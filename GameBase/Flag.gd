72d159aaeb6dbe72a8488205c493905c253b80d8
saimukzi <saimukzi@proton.me>
2022-10-23 22:56:02 +0800

    FlagControl to Flag

--- GameBase/Flag.gd
+++ GameBase/Flag.gd
@@ -1,4 +1,4 @@
-extends PathFollow2D
+extends Node2D
 
 export(NodePath) var anthem_system
 onready var anthem_system_node = get_node(anthem_system)
@@ -7,4 +7,4 @@ export(Global.Flag) var flag = Global.Flag.LEFT
 
 func _process(_delta):
 	# print(anthem_system_node.get_flag_height(AnthemSystem.Flag.LEFT))
-	unit_offset = anthem_system_node.get_flag_height(Global.Flag.LEFT)
+	$FlagPath/FlagControl.unit_offset = anthem_system_node.get_flag_height(Global.Flag.LEFT)
