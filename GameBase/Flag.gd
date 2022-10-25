2fd5b0b174b229ee8a761b2f03f9c1bf83f950c9
saimukzi <saimukzi@proton.me>
2022-10-25 23:27:30 +0800

    Flag state machine, not completed

--- GameBase/Flag.gd
+++ GameBase/Flag.gd
@@ -8,3 +8,22 @@ func _ready():
 
 func update_flag(height):
 	$FlagPath/FlagControl.unit_offset = height
+
+class MyState extends StateMachine.State:
+	var flag_node
+	func init():
+		flag_node = state_machine.flag_node
+
+class Idle extends MyState:
+	pass
+
+class MyStateMachine extends StateMachine:
+	var flag_node
+	func _init(flag_node):
+		self.flag_node = flag_node
+	func default_state():
+		return MyState.new()
+
+onready var state_machine = MyStateMachine.new(self)
+func state():
+	return state_machine.state
