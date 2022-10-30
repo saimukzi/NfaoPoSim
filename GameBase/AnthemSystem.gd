821de3a72fbce0bc5c36b59cff62a8b6459dc6c5
saimukzi <saimukzi@proton.me>
2022-10-30 22:54:40 +0800

    move part of anthem logic to flag

--- GameBase/AnthemSystem.gd
+++ GameBase/AnthemSystem.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export(bool) var auto_play = true
+export(float) var flag_prepare_sec = 0.5
 export(float) var flag_down_sec = 0.5
 export(float) var idle_sec_min = 3.0
 export(float) var idle_sec_max = 6.0
@@ -9,100 +10,21 @@ var flag_node_ary = []
 
 onready var rand = $"/root/Runtime".rand
 onready var timer = $Timer
-onready var audio_player = $AudioStreamPlayer
-onready var state_machine = MyStateMachine.new(self)
-onready var audio_length = audio_player.stream.get_length()
 
 signal anthem_end()
 
 func _ready():
 	if auto_play:
-		state_machine.set_next_state(Idle.new())
-	else:
-		state_machine.set_next_state(Stop.new())
+		$Timer.start(rand.randf_range(idle_sec_min,idle_sec_max))
 
 func reg_flag(flag_node):
 	flag_node_ary.append(flag_node)
-
-func _physics_process(_delta):
-	state_machine.tick()
-
-func _process(delta):
-	state()._process(delta)
+	flag_node.connect("flag_done",self,"_on_flag_done")
 
 func _on_Timer_timeout():
-	state()._on_Timer_timeout()
+	var flag_node = flag_node_ary[rand.randi_range(0,flag_node_ary.size()-1)]
+	flag_node.play()
 
-func _on_AudioStreamPlayer_finished():
-	state()._on_AudioStreamPlayer_finished()
-
-class MyState extends StateMachine.State:
-	var anthem_system
-	func init():
-		anthem_system = state_machine.anthem_system
-	func is_playing():
-		return false
-	func _process(_delta):
-		pass
-	func _on_Timer_timeout():
-		pass
-	func _on_AudioStreamPlayer_finished():
-		pass
-
-class Stop extends MyState:
-	pass
-
-class Idle extends MyState:
-	func start():
-		anthem_system.timer.start(anthem_system.rand.randf_range(anthem_system.idle_sec_min,anthem_system.idle_sec_max))
-	func _on_Timer_timeout():
-		if anthem_system.auto_play:
-			set_next_state(FlagDown.new())
-		else:
-			set_next_state(Stop.new())
-
-class FlagDown extends MyState:
-	var flag_node
-	func start():
-		var flag_node_ary = anthem_system.flag_node_ary
-		flag_node = flag_node_ary[anthem_system.rand.randi_range(0,flag_node_ary.size()-1)]
-		anthem_system.timer.start(anthem_system.flag_down_sec)
-	func _process(_delta):
-		var ret = anthem_system.timer.time_left/anthem_system.timer.wait_time
-		ret = clamp(ret,0,1)
-		flag_node.update_flag(ret)
-	func _on_Timer_timeout():
-		set_next_state(FlagUp.new(flag_node))
-	func end():
-		flag_node.update_flag(0)
-
-class FlagUp extends MyState:
-	var flag_node
-	func _init(flag_node).():
-		self.flag_node = flag_node
-	func start():
-		anthem_system.audio_player.play()
-	func _process(_delta):
-		var ret = anthem_system.audio_player.get_playback_position()
-		ret /= anthem_system.audio_length
-		ret = clamp(ret,0,1)
-		flag_node.update_flag(ret)
-	func _on_AudioStreamPlayer_finished():
-		set_next_state(Idle.new())
-	func is_playing():
-		return true
-	func end():
-		flag_node.update_flag(1)
-
-class MyStateMachine extends StateMachine:
-	var anthem_system
-	func _init(_anthem_system):
-		self.anthem_system = _anthem_system
-	func default_state():
-		return MyState.new()
-
-func state():
-	return state_machine.state
-
-func is_playing():
-	return state().is_playing()
+func _on_flag_done():
+	if auto_play:
+		$Timer.start(rand.randf_range(idle_sec_min,idle_sec_max))
--- GameBase/Flag.gd
+++ GameBase/Flag.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal flag_done()
+
 export(NodePath) var anthem_system
 onready var anthem_system_node = get_node(anthem_system)
 
@@ -9,13 +11,57 @@ func _ready():
 func update_flag(height):
 	$FlagPath/FlagControl.unit_offset = height
 
+func play():
+	state_machine.set_next_state(FlagPrepare.new())
+
+func _on_PrepareTimer_timeout():
+	state()._on_PrepareTimer_timeout()
+
+func _on_AudioStreamPlayer2D_finished():
+	state()._on_AudioStreamPlayer2D_finished()
+
+func _physics_process(_delta):
+	state_machine.tick()
+
+func _process(delta):
+	state()._process(delta)
+
 class MyState extends StateMachine.State:
 	var flag_node
 	func init():
-		flag_node = state_machine.flag_node
+		self.flag_node = state_machine.flag_node
+	func _process(delta):
+		pass
+	func _on_PrepareTimer_timeout():
+		pass
+	func _on_AudioStreamPlayer2D_finished():
+		pass
 
 class Idle extends MyState:
 	pass
+
+class FlagPrepare extends MyState:
+	func start():
+		flag_node.get_node('PrepareTimer').start(flag_node.anthem_system_node.flag_prepare_sec)
+	func _on_PrepareTimer_timeout():
+		set_next_state(FlagUp.new())
+
+class FlagUp extends MyState:
+	var player
+	func start():
+		self.player = flag_node.get_node('AudioStreamPlayer2D')
+		self.player.play()
+	func _process(_delta):
+		var ret = self.player.get_playback_position()
+		ret /= player.stream.get_length()
+		ret = clamp(ret,0,1)
+		flag_node.update_flag(ret)
+	func _on_AudioStreamPlayer2D_finished():
+		set_next_state(Idle.new())
+	func end():
+		self.player.seek(0)
+		flag_node.update_flag(1)
+		flag_node.emit_signal("flag_done")
 
 class MyStateMachine extends StateMachine:
 	var flag_node
