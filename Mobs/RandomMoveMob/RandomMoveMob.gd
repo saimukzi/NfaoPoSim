5996bc6fa3e917d87af3172382601e65e81fbfd0
saimukzi <saimukzi@proton.me>
2022-11-01 22:48:16 +0800

    mob look at flag when flag raise

--- Mobs/RandomMoveMob/RandomMoveMob.gd
+++ Mobs/RandomMoveMob/RandomMoveMob.gd
@@ -4,30 +4,57 @@ export(NodePath) var anthem_system
 onready var anthem_system_node = get_node(anthem_system)
 
 var speed = 50
-var timeout = 0.0
 
 func _ready():
 	anthem_system_node.connect('flag_start',self,'_on_flag_start')
 	anthem_system_node.connect('flag_done',self,'_on_flag_done')
+	state_machine.set_next_state(NormalState.new())
+
+class MyState extends StateMachine.State:
+	var me
+	func init():
+		self.me = state_machine.me
+	func _physics_process(delta): pass
+	func _on_flag_start(flag_node): pass
+	func _on_flag_done(flag_node): pass
 
 func _physics_process(delta):
-	while delta > 0:
-		if timeout <= 0:
-			rotation = rand_range(0,2*PI)
-			timeout += rand_range(0,2)
-		var process_time = min(delta, timeout)
-		if process_time > 0:
-			var move_v2 = Vector2.RIGHT.rotated(rotation)
-			move_v2 *= speed
-			move_v2 *= process_time
-			position += move_v2
-			delta -= process_time
-			timeout -= process_time
+	state_machine.tick()
+	state()._physics_process(delta)
+func _on_flag_start(flag_node): state()._on_flag_start(flag_node)
+func _on_flag_done(flag_node): state()._on_flag_done(flag_node)
 
-func _on_flag_start(flag_node):
-	print('_on_flag_start')
-	pass
+class NormalState extends MyState:
+	var rotate_timeout = 0
+	func _physics_process(delta):
+		while delta > 0:
+			if rotate_timeout <= 0:
+				me.rotation = rand_range(0,2*PI)
+				rotate_timeout += rand_range(1,2)
+			var process_time = min(delta, rotate_timeout)
+			if process_time > 0:
+				var move_v2 = Vector2.RIGHT.rotated(me.rotation)
+				move_v2 *= me.speed
+				move_v2 *= process_time
+				me.position += move_v2
+				delta -= process_time
+				rotate_timeout -= process_time
+	func _on_flag_start(flag_node):
+		state_machine.set_next_state(FlagState.new())
 
-func _on_flag_done(flag_node):
-	print('_on_flag_done')
-	pass
+class FlagState extends MyState:
+	func start():
+		me.rotation = PI
+	func _on_flag_done(flag_node):
+		state_machine.set_next_state(NormalState.new())
+
+class MyStateMachine extends StateMachine:
+	var me
+	func _init(me):
+		self.me = me
+	func default_state():
+		return MyState.new()
+
+onready var state_machine = MyStateMachine.new(self)
+func state():
+	return state_machine.state
