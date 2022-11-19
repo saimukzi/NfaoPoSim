7e91de2e12ab12fb4abd97b86fd38c5a2c085862
saimukzi <saimukzi@proton.me>
2022-11-19 22:17:46 +0800

    fix script

--- SplashScene/SplashScene.gd
+++ SplashScene/SplashScene.gd
@@ -1,13 +1,15 @@
 extends Node
 
-signal finish()
+signal done()
 
 func _ready():
 	pass
 
 func next_input_pressed():
-  if Input.is_action_pressed("ui_ok"):
-	return true
+	if Input.is_action_pressed("ui_ok"):
+		return true
+	return false
+
 
 class MyState extends StateMachine.State:
 	var me
@@ -15,29 +17,29 @@ class MyState extends StateMachine.State:
 
 class InitState extends MyState:
 	func start():
-		me.$Developer.hide()
-		me.$Disclaimer.hide()
+		me.getNode('Developer').hide()
+		me.getNode('Disclaimer').hide()
 
 class DeveloperState extends MyState:
 	func start():
-		me.$Developer.show()
-		me.$Disclaimer.hide()
+		me.getNode('Developer').show()
+		me.getNode('Disclaimer').hide()
 	func _process(delta):
 		if me.next_input_pressed():
 			set_next_state(DisclaimerState.new())
 
 class DisclaimerState extends MyState:
 	func start():
-		me.$Developer.hide()
-		me.$Disclaimer.show()
+		me.getNode('Developer').hide()
+		me.getNode('Disclaimer').show()
 	func _process(delta):
 		if me.next_input_pressed():
 			set_next_state(EndState.new())
 
 class EndState extends MyState:
 	func start():
-		me.$Developer.hide()
-		me.$Disclaimer.hide()
+		me.getNode('Developer').hide()
+		me.getNode('Disclaimer').hide()
 		me.emit_signal('finish')
 
 class MyStateMachine extends StateMachine:
